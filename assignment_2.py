a34c36b79dd1cf2a304f5c928d1e0d08ce59652d
BhumikaAgre <bhumika.a1213@gmail.com>
2025-12-22 19:50:25 +0530

    Fix sum calculation and print statement in assignment_2.py

--- assignment_2.py
+++ assignment_2.py
@@ -11,3 +11,10 @@ for i in range(1,51):
     sum=sum+i
 
     print("The sum of integers from 1 to 50 is:",sum)
+
+Task 2
+
+total = 0
+for i in range(1, 51):
+    total = total + i
+print("The sum of integers from 1 to 50 is:", total)
